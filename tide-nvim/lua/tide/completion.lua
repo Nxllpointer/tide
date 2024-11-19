7ad9dfb19ce228544b6ee13d05b56abe94de7698
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2024-11-19 21:16:54 +0100

    Update tinymist-rnote

--- tide-nvim/lua/tide/completion.lua
+++ tide-nvim/lua/tide/completion.lua
@@ -5,7 +5,7 @@ lze.load {
     require("blink.cmp").setup {
       keymap = require("tide.mappings").blink(),
       trigger = { signature_help = { enabled = true } },
-      accept = { auto_brackets = { enabled = true } },
+      -- accept = { auto_brackets = { enabled = true } },
     }
   end
 }
