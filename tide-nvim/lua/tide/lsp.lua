ff8902ecd33d153da1dd0eb1c846d67c516508e8
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2024-11-27 21:25:05 +0100

    Add border to lsp windows

--- tide-nvim/lua/tide/lsp.lua
+++ tide-nvim/lua/tide/lsp.lua
@@ -1,11 +1,20 @@
+local BORDER = "rounded"
+
 vim.lsp.handlers[vim.lsp.protocol.Methods.textDocument_hover] = function(err, result, ctx)
   local buffer, window = vim.lsp.handlers.hover(err, result, ctx, {
-    wrap = false
+    wrap = false,
+    border = BORDER
   })
 
   if buffer and window then
     vim.api.nvim_win_set_width(window, vim.api.nvim_win_get_width(window) + 2) -- Add space for signcolumn
   end
 end
+
+vim.diagnostic.config {
+  float = {
+    border = BORDER
+  }
+}
 
 vim.lsp.inlay_hint.enable(true)
