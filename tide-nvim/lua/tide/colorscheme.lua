1eb2c36c9c72354a988176072f4cb96b62c83f57
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2024-11-27 20:16:45 +0100

    Enable lsp catppuccin integration

--- tide-nvim/lua/tide/colorscheme.lua
+++ tide-nvim/lua/tide/colorscheme.lua
@@ -13,7 +13,27 @@ lze.load {
       default_integrations = false,
       integrations = {
         blink_cmp = true,
-        neotree = true
+        neotree = true,
+        native_lsp = { -- Having to specify all values is considered a bug https://github.com/catppuccin/nvim/issues/763
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+            ok = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+            ok = { "underline" },
+          },
+          inlay_hints = {
+            background = false,
+          }
+        }
       },
     })
 
