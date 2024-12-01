0ae5ad0bc781479bc132ff8c499e8d32507e796b
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2024-12-01 17:08:37 +0100

    Add keybind to exit terminal mode

--- tide-nvim/lua/tide/mappings.lua
+++ tide-nvim/lua/tide/mappings.lua
@@ -10,13 +10,16 @@ function M.other()
     { "<leader>b", function() vim.cmd.Telescope("buffers") end, desc = "List Buffers" },
     { "<leader>g", function() vim.cmd.Telescope("live_grep") end, desc = "Grep Working Directory" },
     { "<leader>r", function() vim.cmd.Telescope("resume") end, desc = "Resume Telescope" },
+
     { "<leader>c", group = "Code" },
     { "<leader>ca", vim.lsp.buf.code_action, desc = "Action" },
     { "<leader>cr", vim.lsp.buf.rename, desc = "Rename" },
     { "<leader>cf", vim.lsp.buf.format, desc = "Format" },
     { "<leader>cd", function() vim.cmd.Telescope("lsp_definitions") end, desc = "Definitions" },
     { "<leader>cu", function() vim.cmd.Telescope("lsp_references") end, desc = "Usages" },
-    { "<leader>cs", function() vim.cmd.Telescope("lsp_document_symbols") end, desc = "Symbols" }
+    { "<leader>cs", function() vim.cmd.Telescope("lsp_document_symbols") end, desc = "Symbols" },
+
+    { "<C-Space>", vim.cmd.stopinsert, desc = "Exit terminal mode", mode = "t" }
   }
 end
 
