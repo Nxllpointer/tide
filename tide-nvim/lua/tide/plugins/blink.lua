693c6b6bb01ff3ae2b1b85c8fb8a7eac7958e660
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2025-01-04 15:01:01 +0100

    Fix blink.cmp

--- tide-nvim/lua/tide/plugins/blink.lua
+++ tide-nvim/lua/tide/plugins/blink.lua
@@ -4,11 +4,19 @@ return {
   on_require = "blink.cmp",
   after = function()
     require("blink.cmp").setup {
+      fuzzy = {
+        prebuilt_binaries = {
+          download = false
+        }
+      },
+
       keymap = require("tide.mappings").blink(),
-      trigger = { signature_help = { enabled = true } },
-      windows = {
-        autocomplete = {
-          selection = "manual"
+
+      signature = { enabled = true },
+
+      completion = {
+        list = {
+          selection = "preselect";
         }
       }
     }
